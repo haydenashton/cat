c307d72885b32ffe82aff171e306a6ac3ea936a3
Hayden Ashton <haydenashton@hotmail.com>
2013-04-09 20:07:32 +1200

    Add extra options. -s to suppress extra blank lines and -b to number non-blank lines

--- cat.py
+++ cat.py
@@ -9,17 +9,27 @@ class CatCommand:
   def run(self, input, options):
     #default options
     e = ""
+    blank_count = 0
     
     for line in input:
       #modify printed line according to options
-      if options.showend:
-        line = line.rstrip()
-        e = "$\n"
-      if options.shownum:
-        line = "{0} {1}".format(self.count, line)
+      if options.squeezeblank:
+        if not line.strip():
+          blank_count += 1
+        else:
+          blank_count = 0
+      if blank_count < 2:
+        if options.showend:
+          line = line.rstrip()
+          e = "$\n"
+        if options.shownum:
+          line = "{0} {1}".format(self.count, line)
+        if options.shownumnonempty:
+          if line.strip():
+            line = "{0} {1}".format(self.count, line)
 
-      self.count += 1
-      print(line, end=e)
+        self.count += 1
+        print(line, end=e)
 
 
 def main():
@@ -29,6 +39,8 @@ def main():
 
   parser.add_option("-E", dest="showend", action="store_true", help="Show $ at line endings")
   parser.add_option("-n", dest="shownum", action="store_true", help="Show line numbers")
+  parser.add_option("-b", dest="shownumnonempty", action="store_true", help="Show line numbers on non-empty lines")
+  parser.add_option("-s", dest="squeezeblank", action="store_true", help="Remove repeated blank lines")
   (options, args) = parser.parse_args()
 
   cat = CatCommand()
